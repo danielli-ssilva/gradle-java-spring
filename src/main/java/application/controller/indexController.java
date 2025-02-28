6b671423178099a4a9937eb52e238039bbd4e769
Danielli Silva <164676910+danielli-ssilva@users.noreply.github.com>
2025-02-28 19:57:44 +0000

    aula 28/02

---
+++ src/main/java/application/controller/indexController.java
@@ -0,0 +1,34 @@
+package application.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class indexController {
+    @RequestMapping("/")
+    public String home(){
+        return "index.jsp";
+    }
+
+    @RequestMapping("/mensagem")
+    public String mensagem (@RequestParam int opcao, Model ui) {
+        String texto = "";
+        switch (opcao) {
+            case 1:
+                texto = "bom dia!";
+                break;
+            case 2:
+                texto = "boa tarde!";
+                break;
+            case 3:
+                texto = "boa noite!";
+                break;
+            default:
+                texto = "olá";
+                break;
+        }
+        ui.addAttribute("texto", texto);
+        return "mensagem.jsp";
+    }
+}
